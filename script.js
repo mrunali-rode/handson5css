d8de523ad16114f301ab2039ba58ffd278abe344
mrunali-rode <mrunalirode2211@gmail.com>
2022-11-26 00:19:58 +0530

    modified

---
+++ script.js
@@ -0,0 +1,84 @@
+// function div(){
+//     var num1=10;
+//     var num2=2;
+//     var division=num1/num2;
+//     return division;
+    
+
+// }
+// console.log(div());
+// function pro(){
+//     var num1=10;
+//     var num2=2;
+//     var product=num1*num2;
+//     return product;
+
+// }
+// console.log(pro());
+// function modul(){
+//     var num1=10;
+//     var num2=2;
+//     var mod=num1%num2;
+//     return mod;
+
+// }
+// console.log(modul());
+//Es5 function
+// function number(n){
+//   if(n%3==0)
+//   {
+//     console.log("fizz");
+//   }
+//   else if(n%5==0)
+//   {
+//     console.log("buzz");
+//   }
+//   else if(n%3==0 && n%5==0)
+//   {
+//     console.log("fizz-buzz");
+//   }
+
+// }
+
+// number(15);
+//Arrow function
+//  function number(a,b,c,d,e){
+//   var sum=a+b+c+d+e;
+//   console.log(sum);
+    
+//  }
+
+//  number(10,10,5,20,4);
+
+//  const  number1=(a,b,c,d,e)=>{
+//   var sum=a+b+c+d+e;
+//   console.log(sum);
+    
+//  }
+
+//  number1(20,10,5,20,4);
+function number(){
+  var a=2;
+  var b=5;
+  var c=10;
+  var d=20;
+  var e=10;
+  var sum=a+b+c+d+e;
+  return(sum);
+    
+ }
+ console.log(number());
+
+ const  number1=()=>{
+  var a=4;
+  var b=5;
+  var c=10;
+  var d=20;
+  var e=10;
+
+  var sum=a+b+c+d+e;
+  return(sum);
+    
+ }
+
+ console.log(number1());
